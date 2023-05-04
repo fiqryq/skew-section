3b3e98d0d93ddcd29199246dcc088a46bdd6c7af
fiqryq <fiqrychoerudin48@gmail.com>
2023-05-04 08:35:34 +0700

    feat: finish site

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-const button = document.querySelector('#button');
-const text = document.querySelector('#sometext');
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
-button.addEventListener(
-  'click',
-  () => (text.innerHTML = 'This text generated form button create.')
-);
+gsap.registerPlugin(ScrollTrigger);
+
+let proxy = { skew: 0 },
+  skewSetter = gsap.quickSetter('.skew-container', 'skewY', 'deg'),
+  clamp = gsap.utils.clamp(-10, 10);
+
+ScrollTrigger.create({
+  onUpdate: (self) => {
+    let skew = clamp(self.getVelocity() / -300);
+    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+      proxy.skew = skew;
+      gsap.to(proxy, {
+        skew: 0,
+        duration: 0.5,
+        ease: 'power3',
+        overwrite: true,
+        onUpdate: () => skewSetter(proxy.skew),
+      });
+    }
+  },
+});
+
+ßgsap.set('.skew-container', {
+  transformOrigin: 'right center',
+  force3D: true,
+});
